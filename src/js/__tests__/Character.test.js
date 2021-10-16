bec5e7ed696bcc8504fd5ffc12d9f71f64962247
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-10-16 20:15:51 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -7,7 +7,7 @@ test('Должен создаваться объект класса Character', 
 test('Должна выбрасываться ошибка при некорректном имени героя', () => {
   expect(() => new Character(56, 'Bowman')).toThrowError('Нет имени героя');
   expect(() => new Character('В', 'Bowman')).toThrowError('Слишком короткое имя героя');
-  expect(() => new Character('Васисуалий Лоханкин', 'Bowman')).toThrowError('Слишком длинное имя героя');
+  expect(() => new Character('Васисуалий Лукашев', 'Bowman')).toThrowError('Слишком длинное имя героя');
 });
 
 test('Должна выбрасываться ошибка при несуществующем типе героя', () => {
